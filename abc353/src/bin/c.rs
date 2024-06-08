a031cc7d0026ef8a3319adc5caf0626715cc328f
AttODao <atsuatat@gmail.com>
2024-06-08 23:39:31 +0900

    abc353-c

--- abc353/src/bin/c.rs
+++ abc353/src/bin/c.rs
@@ -1,11 +1,25 @@
 #![allow(non_snake_case)]
 use proconio::{fastout, input};
 
-const MOD: usize = 100000000;
+const M: usize = 100000000;
 
 #[fastout]
 fn main() {
-  input! {a:[usize]}
-  let sum = a.into_iter().fold(0, |s, a| (s + a) % MOD);
-  println!("{}", sum * (sum + MOD - 2) % MOD);
+  input! {n:usize, a:[usize;n]}
+  let mut cnt = vec![0; M];
+  for &a in &a {
+    cnt[a] += 1;
+  }
+  for i in (0..M - 1).rev() {
+    cnt[i] += cnt[i + 1];
+  }
+  let mut c = 0;
+  for &a in &a {
+    c += cnt[M - a];
+    if a * 2 >= M {
+      c -= 1;
+    }
+  }
+  let sum = a.iter().sum::<usize>();
+  println!("{}", sum * (n - 1) - M * c / 2);
 }
